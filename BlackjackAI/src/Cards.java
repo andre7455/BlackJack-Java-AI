2b7f6c08e64deec2c13443d6ebbae0ef5a363ca1
AvanderLugt <42913113+andre7455@users.noreply.github.com>
2023-01-01 02:40:35 +0100

    initial commit with a few idea's how to start this project

---
+++ BlackjackAI/src/Cards.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Cards {
+    public ArrayList<CardTemplate> GenerateDeck(){
+        ArrayList<CardTemplate> deck = new ArrayList<>();
+        String kind[] = {"",""};
+
+        return deck;
+    }
+}
---
+++ BlackjackAI/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    //dealer has to go until 17 or more even if your cards are lower
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
